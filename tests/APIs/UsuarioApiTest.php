9cb6aa028570e7bee4aaa1918b4ead8e2d6e4ec1
Fernando Lima Fernandes <fvlimafernandes@gmail.com>
2019-09-03 16:56:21 -0300

    Corrigindo testes

--- tests/APIs/UsuarioApiTest.php
+++ tests/APIs/UsuarioApiTest.php
@@ -12,22 +12,7 @@ class UsuarioApiTest extends TestCase
 {
     use ApiTestTrait, WithoutMiddleware, DatabaseTransactions;
 
-    /**
-     * @test
-     */
-    public function test_create_usuario()
-    {
-        $usuario = factory(Usuario::class)->make()->toArray();
-
-        $this->response = $this->json(
-            'POST',
-            '/api/usuarios', $usuario
-        );
-
-        $this->assertApiResponse($usuario);
-    }
-
-    /**
+        /**
      * @test
      */
     public function test_read_usuario()
@@ -58,25 +43,5 @@ class UsuarioApiTest extends TestCase
 
         $this->assertApiResponse($editedUsuario);
     }
-
-    /**
-     * @test
-     */
-    public function test_delete_usuario()
-    {
-        $usuario = factory(Usuario::class)->create();
-
-        $this->response = $this->json(
-            'DELETE',
-             '/api/usuarios/'.$usuario->id
-         );
-
-        $this->assertApiSuccess();
-        $this->response = $this->json(
-            'GET',
-            '/api/usuarios/'.$usuario->id
-        );
-
-        $this->response->assertStatus(404);
-    }
+    
 }
