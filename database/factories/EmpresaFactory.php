d7a1c113e3d5b1ba5b3fc5267e68176e0172d0ca
Fernando Lima Fernandes <fvlimafernandes@gmail.com>
2019-09-04 17:14:02 -0300

    Criando seed para empresas

--- database/factories/EmpresaFactory.php
+++ database/factories/EmpresaFactory.php
@@ -2,15 +2,16 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 use App\Models\Empresa;
+use App\Models\Cidade;
 use Faker\Generator as Faker;
 
 $factory->define(Empresa::class, function (Faker $faker) {
     return [
-        'nome' => $faker->word,
-        'email' => $faker->word,
-        'telefone' => $faker->word,
-        'endereco' => $faker->word,
-        'cidade_id' => $faker->randomDigitNotNull,
+        'nome' => $faker->company,
+        'email' => $faker->email,
+        'telefone' => $faker->phoneNumber,
+        'endereco' => $faker->address,
+        'cidade_id' => Cidade::inRandomOrder()->first()->id,
         'created_at' => $faker->date('Y-m-d H:i:s'),
         'updated_at' => $faker->date('Y-m-d H:i:s'),
     ];
