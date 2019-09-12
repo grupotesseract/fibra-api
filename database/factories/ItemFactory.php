73d760dd5125bfc7e0ba4b0bbbe2398d6808cd1b
Fernando Lima Fernandes <fvlimafernandes@gmail.com>
2019-09-12 17:09:29 -0300

    Ajustando factory pra aproximar o valor do campo Qrcode da formatação real

--- database/factories/ItemFactory.php
+++ database/factories/ItemFactory.php
@@ -9,8 +9,8 @@ use Faker\Generator as Faker;
 $factory->define(Item::class, function (Faker $faker) {
 
     return [
-        'nome' => $faker->word,
-        'qrcode' => $faker->word,
+        'nome' => $faker->sentence,
+        'qrcode' => 'FIBRA-'.$faker->randomNumber(4),
         'circuito' => $faker->randomElement(['Normal', 'Emergência']),
         'planta_id' => Planta::inRandomOrder()->first()->id,
         'created_at' => $faker->date('Y-m-d H:i:s'),
