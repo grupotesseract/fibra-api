cba60d8e6a462b72237cad3bf96a87273baf7521
Fernando Lima Fernandes <fvlimafernandes@gmail.com>
2019-09-16 11:27:57 -0300

    Incluindo blade dinâmica de Empresas/Plantas em Programações

--- resources/views/programacoes/fields.blade.php
+++ resources/views/programacoes/fields.blade.php
@@ -25,10 +25,22 @@
 
 
 <!-- Planta Id Field -->
-<div class="form-group col-sm-6">
-    {!! Form::label('planta_id', 'Planta') !!}
-    {!! Form::text('planta_id', null, ['class' => 'form-control']) !!}
-</div>
+<!-- Empresa Id Field -->
+@if (isset($programacao))
+
+    <div class="form-group col-sm-6">
+        @include('plantas.select', [
+            'Model' => $programacao
+        ])
+    </div>    
+
+@else
+
+    <div class="form-group col-sm-6">
+        @include('plantas.select')
+    </div>    
+
+@endif
 
 <!-- Submit Field -->
 <div class="form-group col-sm-12">
