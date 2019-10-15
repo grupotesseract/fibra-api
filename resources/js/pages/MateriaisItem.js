256170cca4bf12c7e8f300481c5858bf28427ad5
Renato Gomes <renatoo.agomes@gmail.com>
2019-10-15 15:29:13 -0300

    Alterando MateriaisItem.js para o padrão es6 / axios.. :+1:

--- resources/js/pages/MateriaisItem.js
+++ resources/js/pages/MateriaisItem.js
@@ -5,65 +5,54 @@ import Axios from 'axios'
  */
 class MateriaisItem {
 
-  /**
-   * @constructor
-   */
-  constructor() {
-    this.eventHandlers()
-  }
+    /**
+     * @constructor
+     */
+    constructor() {
+        this.eventHandlers()
+    }
 
-  /**
-   * @returns
-   */
-  eventHandlers() {
-    $('.select2').select2();
-    $('#form-associar-materiais').submit((event) => {
-      event.preventDefault()
-      this.postAssociarMaterialItem()
-    });
-  }
-
-  static resetFormAssociarItem (select_materiais, input_qnt) {
-    $(select_materiais).val(0).trigger('change')
-    $(input_qnt).val(0);
-  }
-
-  postAssociarMaterialItem() {
-    let url = location.href + "/materiais"
-    let select_materiais = $('#material_id');
-    let input_qnt = $('#qnt_instalada');
-    let container_erros = $("#container-erros");
-    let data = {
-      material_id : select_materiais.val(),
-      qnt_instalada : input_qnt.val(),
-    };
-
-    $.ajaxSetup({
-      headers: {
-        'X-CSRF-TOKEN': $('input[name="_token"]').val()
-      }
-    });
-
-    $.ajax({
-      url: url,
-      type: 'POST',
-      dataType: 'json',
-      data: $.param( data ),
-      beforeSend: function () {
-        container_erros.html('').hide();
-      },
-      success: function (data, textStatus, jqXHR) {
-        console.log('success');
-        MateriaisItem.resetFormAssociarItem(select_materiais, input_qnt);
-        LaravelDataTables.dataTableBuilder.draw();
-      },
-      error: function (jqXHR, textStatus, errorThrown) {
-        $.each(jqXHR.responseJSON.errors, function (key, item) {
-          container_erros.append("<p class=''>"+item+"</p>").slideDown();
+    /**
+     * @returns
+     */
+    eventHandlers() {
+        $('.select2').select2();
+        $('#form-associar-materiais').submit((event) => {
+            event.preventDefault()
+            this.submitFormMaterialItem()
         });
-      }
-    });
-  }
+    }
+
+    resetFormAssociarItem (select_materiais, input_qnt) {
+        $(select_materiais).val(0).trigger('change')
+        $(input_qnt).val(0);
+    }
+
+    async submitFormMaterialItem() {
+        const url = location.href + "/materiais"
+        const select_materiais = $('#material_id');
+        const input_qnt = $('#qnt_instalada');
+        const container_erros = $("#container-erros");
+        const data = {
+            material_id : select_materiais.val(),
+            qnt_instalada : input_qnt.val(),
+        };
+
+        container_erros.slideUp().html('');
+
+        const result = await Axios.post(url, data)
+            .then(response => {
+                if (response.data.success){
+                    this.resetFormAssociarItem(select_materiais, input_qnt)
+                    LaravelDataTables.dataTableBuilder.draw()
+                }
+            })
+            .catch(response => {
+                for (let [key, value] of Object.entries(response.response.data.errors)) {
+                    container_erros.append("<p class=''>"+value+"</p>").slideDown()
+                }
+            })
+    }
 
 }
 
