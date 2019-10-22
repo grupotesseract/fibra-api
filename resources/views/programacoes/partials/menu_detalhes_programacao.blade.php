364ef700415f277cc54f1a5bad0b19eabd38379f
Renato Gomes <renatoo.agomes@gmail.com>
2019-10-21 21:22:06 -0300

    corrigindo show_fields de programacoes e liberacoes de documentos, para chamar os acessores com as datas formatadas

--- resources/views/programacoes/partials/menu_detalhes_programacao.blade.php
+++ resources/views/programacoes/partials/menu_detalhes_programacao.blade.php
@@ -1,8 +1,6 @@
-<div class="col-sm-8 ml-5">
-    <div class="form-group">
-        <a class="btn btn-primary form-control" href="{!! route('programacoes.liberacoesDocumentos', $programacao->id) !!}">
-            <i class="fa fa-clock-o"></i>
-            <span> Liberações de Documentos</span>
-        </a>
-    </div>
+<div class="px-5 py-2">
+    <a class="btn btn-primary form-control" href="{!! route('programacoes.liberacoesDocumentos', $programacao->id) !!}">
+        <i class="fa fa-clock-o"></i>
+        <span> Liberações de Documentos</span>
+    </a>
 </div>
