ac0f61b6d26db45a75a07c76fb83b51ab77048e1
Renato Gomes <renatoo.agomes@gmail.com>
2019-11-03 06:51:34 -0300

    ajustando link 'voltar' no plantas.show

--- resources/views/plantas/show.blade.php
+++ resources/views/plantas/show.blade.php
@@ -16,7 +16,7 @@
                     <div class="card">
                         <div class="card-header">
                             <strong>Detalhes</strong>
-                            <a href="{!! url()->previous() !!}" class="btn btn-ghost-light">Voltar</a>
+                            <a href="{!! route('empresas.show', $planta->empresa_id) !!}" class="btn btn-ghost-light">Voltar</a>
                         </div>
                         <div class="card-body">
                             @include('plantas.show_fields')
