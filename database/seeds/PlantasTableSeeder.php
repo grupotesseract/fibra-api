e3a0ca926c07243817bc959110dc28e89a2c44bd
Fernando Lima Fernandes <fvlimafernandes@gmail.com>
2019-11-13 18:55:29 -0300

    Voltando o seed de plantas pro estado antigo

--- database/seeds/PlantasTableSeeder.php
+++ database/seeds/PlantasTableSeeder.php
@@ -12,21 +12,6 @@ class PlantasTableSeeder extends Seeder
      */
     public function run()
     {
-        factory(App\Models\Planta::class, 15)->create()->each(
-            function ($planta) {
-                $planta->itens()->saveMany(
-                    factory(\App\Models\Item::class, 20)->create()->each(
-                        function ($item) {
-                            $materiaisIds = \App\Models\Material::all()->random(10)->pluck('id');
-                            
-                            foreach ($materiaisIds as $materialId) {
-                                $qtdeInstalada = rand(20, 100);
-                                $item->materiais()->attach($materialId, ['quantidade_instalada' => $qtdeInstalada]);
-                            }
-                        }
-                    )
-                );
-            }
-        ); 
+        factory(App\Models\Planta::class, 30)->create();
     }
 }
