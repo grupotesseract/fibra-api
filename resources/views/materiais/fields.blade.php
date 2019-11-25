09f76ed365d98d5c2ad96ac6854c35e87e98f61f
Fernando Lima Fernandes <fvlimafernandes@gmail.com>
2019-11-25 10:27:21 -0300

    Fix - faltava condição de existencia de objeto material no create de material

--- resources/views/materiais/fields.blade.php
+++ resources/views/materiais/fields.blade.php
@@ -60,14 +60,14 @@
 <div class="form-group col-sm-6">
     {!! Form::label('nome', 'Nome') !!}
     {!! Form::text('nome', null, ['class' => 'form-control nomeMaterial', 
-        'disabled' => isset($material->tipo_material_id) && !is_null($material->tipo_material_id) ?? false]) !!}
+        'disabled' => isset($material) && !is_null($material->tipo_material_id) ?? false]) !!}
 </div>
 
 <!-- tipo_reator_qtde Reator Field -->
 <div class="form-group col-sm-6">
     {!! Form::label('tipo_reator_qtde', 'Tipo de Reator') !!}
     {!! Form::number('tipo_reator_qtde', null, ['class' => 'form-control tipoReator', 
-        'disabled' => is_null($material->tipo_reator_qtde) ?? true]) !!}
+        'disabled' => isset($material) && is_null($material->tipo_reator_qtde) ?? true]) !!}
 </div>
 
 <!-- Submit Field -->
