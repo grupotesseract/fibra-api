29d570bd6964c6484f741ae7cb805ececc16257c
Fernando Lima Fernandes <fvlimafernandes@gmail.com>
2019-12-16 15:26:06 -0300

    Zerando variável itens antes de preencher com os itens da planta

--- app/Transformers/EmpresaTransformer.php
+++ app/Transformers/EmpresaTransformer.php
@@ -24,6 +24,7 @@ class EmpresaTransformer extends TransformerAbstract
             ] : null;
 
             //Itens de uma Planta
+            $itens = [];
             foreach ($planta->itens as $item) {
 
                 //Materiais Instalados de uma Planta
