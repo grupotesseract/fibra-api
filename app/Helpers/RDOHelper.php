6fdd1a1e799c5fecef3c83f65d45100253dddd0a
Renato Gomes <renatoo.agomes@gmail.com>
2020-04-06 16:15:13 -0300

    Incluindo RDOHelper para encapsular a logica de montagem do relatorio diario de obra

---
+++ app/Helpers/RDOHelper.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Helpers;
+
+/**
+ * Classe para intermediar a comunicação com o PhpWord facilitando a construçao
+ * das seções do relatorio diario de obra
+ */
+class RDOHelper extends PhpWordHelper
+{
+    //criarCabecalhoLogo
+    //criarSecaoRetanguloAzul
+    //criarSecaoEquipeCliente
+    //criarSecaoEquipeFibra
+    //criarSecaoDocumentacoesExpedidas
+    //criarSecaoAtividadesRealizadas
+    //criarSecaoProblemasEncontrados
+    //criarSecaoInformacoesAdicionais
+    //criarSecaoObservacoes
+    //criarSecaoFotos
+    //criarSecaoResponsaveis
+}
