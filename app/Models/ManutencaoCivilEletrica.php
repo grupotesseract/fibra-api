5f2b7c0665d1e43de16fa5613a97f5d85e3bc9c6
Fernando Lima Fernandes <fvlimafernandes@gmail.com>
2020-04-28 14:29:11 -0300

    Aplicando mutators nas datas do RDO

--- app/Models/ManutencaoCivilEletrica.php
+++ app/Models/ManutencaoCivilEletrica.php
@@ -118,4 +118,151 @@ class ManutencaoCivilEletrica extends Model
     {
         return $this->hasMany(\App\Models\FotoRdo::class, 'manutencao_id');
     }
+
+    /**
+     * Mutator pra data.
+     *
+     * @param Carbon $value
+     * @return Carbon
+     */
+    public function setDataHoraEntradaAttribute($value)
+    {
+        if (! is_null($value)) {
+            if (strpos($value, 'T')) {
+                $dataDB = \Carbon\Carbon::parse($value)->setTimezone(-3);
+            } else {
+                $dataDB = \Carbon\Carbon::parse($value);
+            }
+        } else {
+            $dataDB = null;
+        }
+
+        $this->attributes['data_hora_entrada'] = $dataDB;
+    }
+
+    /**
+     * Mutator pra data.
+     *
+     * @param Carbon $value
+     * @return Carbon
+     */
+    public function setDataHoraSaidaAttribute($value)
+    {
+        if (! is_null($value)) {
+            if (strpos($value, 'T')) {
+                $dataDB = \Carbon\Carbon::parse($value)->setTimezone(-3);
+            } else {
+                $dataDB = \Carbon\Carbon::parse($value);
+            }
+        } else {
+            $dataDB = null;
+        }
+
+        $this->attributes['data_hora_saida'] = $dataDB;
+    }
+
+    /**
+     * Mutator pra data.
+     *
+     * @param Carbon $value
+     * @return Carbon
+     */
+    public function setDataHoraInicioLemAttribute($value)
+    {
+        if (! is_null($value)) {
+            if (strpos($value, 'T')) {
+                $dataDB = \Carbon\Carbon::parse($value)->setTimezone(-3);
+            } else {
+                $dataDB = \Carbon\Carbon::parse($value);
+            }
+        } else {
+            $dataDB = null;
+        }
+
+        $this->attributes['data_hora_inicio_lem'] = $dataDB;
+    }
+
+    /**
+     * Mutator pra data.
+     *
+     * @param Carbon $value
+     * @return Carbon
+     */
+    public function setDataHoraFinalLemAttribute($value)
+    {
+        if (! is_null($value)) {
+            if (strpos($value, 'T')) {
+                $dataDB = \Carbon\Carbon::parse($value)->setTimezone(-3);
+            } else {
+                $dataDB = \Carbon\Carbon::parse($value);
+            }
+        } else {
+            $dataDB = null;
+        }
+
+        $this->attributes['data_hora_final_lem'] = $dataDB;
+    }
+
+    /**
+     * Mutator pra data.
+     *
+     * @param Carbon $value
+     * @return Carbon
+     */
+    public function setDataHoraInicioLetAttribute($value)
+    {
+        if (! is_null($value)) {
+            if (strpos($value, 'T')) {
+                $dataDB = \Carbon\Carbon::parse($value)->setTimezone(-3);
+            } else {
+                $dataDB = \Carbon\Carbon::parse($value);
+            }
+        } else {
+            $dataDB = null;
+        }
+
+        $this->attributes['data_hora_inicio_let'] = $dataDB;
+    }
+
+    /**
+     * Mutator pra data.
+     *
+     * @param Carbon $value
+     * @return Carbon
+     */
+    public function setDataHoraFinalLetAttribute($value)
+    {
+        if (! is_null($value)) {
+            if (strpos($value, 'T')) {
+                $dataDB = \Carbon\Carbon::parse($value)->setTimezone(-3);
+            } else {
+                $dataDB = \Carbon\Carbon::parse($value);
+            }
+        } else {
+            $dataDB = null;
+        }
+
+        $this->attributes['data_hora_final_let'] = $dataDB;
+    }
+
+    /**
+     * Mutator pra data.
+     *
+     * @param Carbon $value
+     * @return Carbon
+     */
+    public function setDataHoraInicioAtividadesAttribute($value)
+    {
+        if (! is_null($value)) {
+            if (strpos($value, 'T')) {
+                $dataDB = \Carbon\Carbon::parse($value)->setTimezone(-3);
+            } else {
+                $dataDB = \Carbon\Carbon::parse($value);
+            }
+        } else {
+            $dataDB = null;
+        }
+
+        $this->attributes['data_hora_inicio_atividades'] = $dataDB;
+    }
 }
