c42697ac1e62cc14b8798d018738b586ae5fb4ea
Fernando Lima Fernandes <fvlimafernandes@gmail.com>
2020-05-04 22:05:04 +0000

    Apply fixes from StyleCI

--- database/migrations/2020_05_04_183253_altera_campos_nullable_manutencaocivileletrica.php
+++ database/migrations/2020_05_04_183253_altera_campos_nullable_manutencaocivileletrica.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class AlteraCamposNullableManutencaocivileletrica extends Migration
 {
@@ -18,7 +18,7 @@ class AlteraCamposNullableManutencaocivileletrica extends Migration
             $table->text('informacoes_adicionais')->nullable()->change();
             $table->text('observacoes')->nullable()->change();
             $table->string('obra_atividade')->nullable()->change();
-            $table->string('equipe_cliente')->nullable()->change();            
+            $table->string('equipe_cliente')->nullable()->change();
         });
     }
 
