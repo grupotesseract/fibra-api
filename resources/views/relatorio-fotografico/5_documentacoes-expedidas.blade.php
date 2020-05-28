8af8026537af0727f2d6b74c83f8eb007f57bf8e
Fernando Lima Fernandes <fvlimafernandes@gmail.com>
2020-05-28 11:45:04 -0300

    Ajustando parágrafos

--- resources/views/relatorio-fotografico/5_documentacoes-expedidas.blade.php
+++ resources/views/relatorio-fotografico/5_documentacoes-expedidas.blade.php
@@ -7,89 +7,58 @@
                 <tr>
                     <td>
                         <form>
-                            <div class="form-inline">
+                            <div class="form-inline wide">
                                 <label>IT:</label>
-                                <input type="text"
+                                {{-- <input type="text"
                                     class="form-control form-control-plaintext"
-                                    readonly
-                                    value="{{$manutencaoCivilEletrica->it}}"
-                                />
+                                    readonly                                    
+                                    value="{{$manutencaoCivilEletrica->it}}"                                    
+                                /> --}}
+                                <p>{{$manutencaoCivilEletrica->it}}</p>
                             </div>
 
-                            <div class="form-inline">
+                            <div class="form-inline wide">
                                 <label>LEM:</label>
-                                <input type="text"
+                                {{-- <input type="text"
                                     class="form-control form-control-plaintext"
                                     readonly
                                     value="{{$manutencaoCivilEletrica->lem}}"
-                                />
+                                /> --}}
+                                <p>{{$manutencaoCivilEletrica->lem}}</p>
                             </div>
 
-                            <div class="form-inline">
+                            <div class="form-inline wide">
                                 <label>LET:</label>
-                                <input type="text"
+                                {{-- <input type="text"
                                     class="form-control form-control-plaintext"
                                     readonly
                                     value="{{$manutencaoCivilEletrica->let}}"
-                                />
+                                /> --}}
+                                <p>{{$manutencaoCivilEletrica->let}}</p>
                             </div>
 
-                            <div class="form-inline">
+                            <div class="form-inline wide">
                                 <label>OS:</label>
-                                <input type="text"
+                                {{-- <input type="text"
                                     class="form-control form-control-plaintext"
-                                    readonly
+                                    readonly                                    
                                     value="{{$manutencaoCivilEletrica->os}}"
-                                />
+                                /> --}}
+                                <p>{{$manutencaoCivilEletrica->os}}</p>
                             </div>
 
                             <div class="form-inline wide">
-                                <label>Início da Liberação LEM:</label>
-                                <input type="text"
-                                    class="form-control form-control-plaintext"
-                                    size="1"
-                                    readonly
-                                    value="{{! is_null($manutencaoCivilEletrica->data_hora_inicio_lem) ? $manutencaoCivilEletrica->data_hora_inicio_lem->format('H:i') : '    '}}"
-
-                                />                                
-
-                                <label>Término da Liberação:</label>
-                                <input type="text"
-                                    class="form-control form-control-plaintext"
-                                    size="1"
-                                    readonly
-                                    value="{{! is_null($manutencaoCivilEletrica->data_hora_final_lem) ? $manutencaoCivilEletrica->data_hora_final_lem->format('H:i') : '    '}}"
-                                />                                
+                                <p><b>Início da Liberação LEM:</b> {{! is_null($manutencaoCivilEletrica->data_hora_inicio_lem) ? $manutencaoCivilEletrica->data_hora_inicio_lem->format('H:i') : '    '}}</p>
+                                <p>&nbsp;&nbsp;<b>Término da Liberação:</b> {{! is_null($manutencaoCivilEletrica->data_hora_final_lem) ? $manutencaoCivilEletrica->data_hora_final_lem->format('H:i') : '    '}}</p>
                             </div>
 
                             <div class="form-inline wide">
-                                <label>Início da Liberação LET:</label>
-                                <input type="text"
-                                    class="form-control form-control-plaintext"
-                                    size="1"
-                                    readonly
-                                    value="{{! is_null($manutencaoCivilEletrica->data_hora_inicio_let) ? $manutencaoCivilEletrica->data_hora_inicio_let->format('H:i') : '    '}}"
-
-                                />                                
-
-                                <label>Término da Liberação:</label>
-                                <input type="text"
-                                    class="form-control form-control-plaintext"
-                                    size="1"
-                                    readonly
-                                    value="{{! is_null($manutencaoCivilEletrica->data_hora_final_let) ? $manutencaoCivilEletrica->data_hora_final_let->format('H:i') : '    '}}"
-                                />                                
+                                <p><b>Início da Liberação LET:</b> {{! is_null($manutencaoCivilEletrica->data_hora_inicio_let) ? $manutencaoCivilEletrica->data_hora_inicio_let->format('H:i') : '    '}}</p>
+                                <p>&nbsp;&nbsp;<b>Término da Liberação:</b> {{! is_null($manutencaoCivilEletrica->data_hora_final_let) ? $manutencaoCivilEletrica->data_hora_final_let->format('H:i') : '    '}}</p>
                             </div>
 
                             <div class="form-inline">
-                                <label>Início da Atividade:</label>
-                                <input type="text"
-                                    class="form-control form-control-plaintext"
-                                    size="1"
-                                    readonly
-                                    value="{{! is_null($manutencaoCivilEletrica->data_hora_inicio_atividades) ? $manutencaoCivilEletrica->data_hora_inicio_atividades->format('H:i') : '    '}}"
-                                />
-                                
+                                <p><b>Início da Atividade:</b> {{! is_null($manutencaoCivilEletrica->data_hora_inicio_atividades) ? $manutencaoCivilEletrica->data_hora_inicio_atividades->format('H:i') : '    '}}</p>
                             </div>
                         </form>
                     </td>
